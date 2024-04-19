9574ebc849bb12c7418806a2d43adf5bf11d80a0
vukani-dev <vukani@posteo.net>
2024-04-19 18:22:16 -0400

    I dont use dmenu standalone.

--- modules/suckless/default.nix
+++ modules/suckless/default.nix
@@ -1,24 +1,8 @@
 {...}: {
   nixpkgs.config.packageOverrides = pkgs: {
-    dmenu = pkgs.dmenu.overrideAttrs (oldAttrs: {
-      src = /home/vukani/.suckless/dmenu;
-      # src = pkgs.fetchFromGitHub {
-      #   owner = "vukani";
-      #   repo = "dwm";
-      #   rev = "4ec69288e9fe723afdf27d18681542f67fbb71e0";
-      # };
-      # For `dmenu`, `conf` can't be used because the derivation doesn't support it
-      postPatch =
-        oldAttrs.postPatch
-        + ''
-          cp ${pkgs.writeText "config.dmenu.h" (builtins.readFile ./config.dmenu.h)} config.h
-        '';
-    });
-
     dwm =
       (pkgs.dwm.overrideAttrs (oldAttrs: {
         buildInputs = oldAttrs.buildInputs ++ [pkgs.xorg.libXcursor pkgs.imlib2 pkgs.xorg.libXinerama pkgs.xorg.libXft pkgs.xorg.libXext pkgs.fontconfig pkgs.pkg-config];
-        # src = /home/vukani/.suckless/dwm;
         src = pkgs.fetchFromGitHub {
           owner = "vukani-dev";
           repo = "dwm";
@@ -34,7 +18,6 @@
       (pkgs.st.overrideAttrs
         (oldAttrs: {
           buildInputs = oldAttrs.buildInputs ++ [pkgs.xorg.libXcursor pkgs.harfbuzz];
-          # src = /home/vukani/.suckless/st;
           src = pkgs.fetchFromGitHub {
             owner = "vukani-dev";
             repo = "st";
