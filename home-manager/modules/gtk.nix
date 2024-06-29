5dfe1f45de2a999b71aabf79d3191c987364e6da
vukani-dev <vukani@posteo.net>
2024-06-29 09:37:35 -0400

    new firefox styles

--- home-manager/modules/gtk.nix
+++ home-manager/modules/gtk.nix
@@ -2,13 +2,8 @@
   gtk = {
     enable = true;
     theme = {
-      name = "Catppuccin-Macchiato-Compact-Pink-Dark";
-      package = pkgs.catppuccin-gtk.override {
-        accents = ["pink"];
-        size = "compact";
-        tweaks = ["rimless" "black"];
-        variant = "macchiato";
-      };
+      name = "pop-gtk-theme";
+      package = pkgs.pop-gtk-theme;
     };
   };
 }
