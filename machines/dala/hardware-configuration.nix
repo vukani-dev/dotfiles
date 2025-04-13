4a471bb1928af59c7f0c794627cdba07fb29c8d2
vukani-dev <vukani@posteo.net>
2025-04-13 11:59:32 -0400

    storage

--- machines/dala/hardware-configuration.nix
+++ machines/dala/hardware-configuration.nix
@@ -33,37 +33,26 @@
     fsType = "vfat";
   };
 
-  fileSystems."/mnt/mwendo" = {
-    device = "//10.1.0.4/mwendo";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-service"];
+  fileSystems."/mnt/storage" = {
+    device = "10.1.0.40:/mnt/storage";
+    fsType = "nfs";
+    options = [
+      "noatime"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=600"
+    ];
   };
 
-  fileSystems."/mnt/music" = {
-    device = "//10.1.0.4/music";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-service"];
+  fileSystems."/mnt/vault" = {
+    device = "10.1.0.40:/mnt/vault";
+    fsType = "nfs";
+    options = [
+      "noatime"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=600"
+    ];
   };
 
-  fileSystems."/mnt/jukwaa" = {
-    device = "//10.1.0.4/jukwaa";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-service"];
-  };
-
-  fileSystems."/mnt/photos" = {
-    device = "//10.1.0.4/photos";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-service"];
-  };
   swapDevices = [
     {device = "/dev/disk/by-uuid/078a46a9-bfa8-4c11-8a1e-9bf6a9f66f68";}
   ];
